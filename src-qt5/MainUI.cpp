ae26c72a2514f4718d14ac3c979d7020df5c3e20
Ken Moore <ken@pcbsd.org>
2016-08-31 15:17:41 -0400

    Also fix the variable-DPI setting in pc-webkitviewer - now it functions properly.

--- src-qt5/MainUI.cpp
+++ src-qt5/MainUI.cpp
@@ -64,12 +64,12 @@ MainUI::MainUI(bool debugmode, QString fileURL, QString title, QString iconpath)
       group_search->layout()->addItem(new QSpacerItem(0,0,QSizePolicy::Expanding, QSizePolicy::Minimum) );
   //Setup the Main Interface
     webview = new QWebView(this);
-      //Fix the font DPI within the webview to match the current screen settings
-      QWebSettings::globalSettings()->setAttribute(QWebSettings::ZoomTextOnly,true);
-      webview->setZoomFactor( QApplication::desktop()->screen()->logicalDpiX()/96.0 );
     this->centralWidget()->layout()->addWidget(webview);
     if(webview->page()==0){ webview->setPage(new QWebPage(webview)); }
     webview->page()->setLinkDelegationPolicy(QWebPage::DelegateAllLinks);
+      //Fix the font DPI within the webview to match the current screen settings
+      QWebSettings::globalSettings()->setAttribute(QWebSettings::ZoomTextOnly,true);
+      webview->setZoomFactor( QApplication::desktop()->screen()->logicalDpiX()/96.0 );
     this->centralWidget()->layout()->addWidget(group_search);
   
   //Make sure the search bar is hidden to start with    
